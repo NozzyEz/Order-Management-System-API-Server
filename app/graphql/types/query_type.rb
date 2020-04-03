9cb768d7ca5538c5fd2a2f95d2b1494f15a5c13c
Mark Sahlgreen <mark@nozzy.org>
2020-04-03 12:48:28 +0200

    added fields for the QueryType so that the database can be queried

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,12 +2,23 @@ module Types
   class QueryType < Types::BaseObject
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
+    
+    # Field to return all users
+    field :users, [Types::UserType], null: false
+    
+    # field to return specific user with the provided user id
+    field :user, Types::UserType, null: false do
+      argument :id, ID, required: true
+    end
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    # Method to fetch all users
+    def users
+      User.all
+    end
+
+    # fetch a user with a user id
+    def user(id:)
+      User.find(id)
     end
   end
 end
