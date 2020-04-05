23c58fbfb8d83333c141fa882e95e9545aee1659
Mark Sahlgreen <mark@nozzy.org>
2020-04-06 00:58:25 +0200

    Enabled quering for single post, also enabled nested user field on post(s)

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -1,7 +1,10 @@
 module Types
   class PostType < Types::BaseObject
-    field :id,        Integer,  null: false
-    field :title,     String,   null: false
-    field :body,      String,   null: false
+    field :id,        Integer,            null: false
+    field :user_id,   Integer,            null: false
+    field :title,     String,             null: false
+    field :body,      String,             null: false
+    field :user,      Types::UserType,    null: false, method: :user
+
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,20 +5,32 @@ module Types
     
     # Field to return all users
     field :users, [Types::UserType], null: false
+    field :posts, [Types::PostType], null: false
     
     # field to return specific user with the provided user id
     field :user, Types::UserType, null: false do
       argument :id, ID, required: true
+    end
+    
+    field :post, Types::PostType, null: false do
+      argument :id, Integer, required: true
     end
 
     # Method to fetch all users
     def users
       User.all
     end
+    def posts
+      Post.all
+    end
 
     # fetch a user with a user id
     def user(id:)
       User.find(id)
+    end
+
+    def post(id:)
+      Post.find(id)
     end
   end
 end
