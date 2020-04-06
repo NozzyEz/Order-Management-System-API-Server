ceb4ae1481e4fd9d3292c79005d6e95fd89bc2a8
Mark Sahlgreen <mark@nozzy.org>
2020-04-06 18:58:45 +0200

    added comment in PostType

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -4,6 +4,7 @@ module Types
     field :user_id,   Integer,            null: false
     field :title,     String,             null: false
     field :body,      String,             null: false
+    # field user to be queried, specify user and the method used to receive the object   
     field :user,      Types::UserType,    null: false, method: :user
 
   end
