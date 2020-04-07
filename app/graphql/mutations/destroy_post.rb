ec23e5c59a1227fd17d8566269cfecde48ff1e40
Mark Sahlgreen <mark@nozzy.org>
2020-04-07 12:59:49 +0200

    finished destroyPost

--- app/graphql/mutations/destroy_post.rb
+++ app/graphql/mutations/destroy_post.rb
@@ -1,11 +1,12 @@
 class Mutations::DestroyPost < Mutations::BaseMutation
     argument :id, Integer, required: true
 
-    field :post,    Types::PostType,    null: true
-    field :errors,  [String],           null: true
+    # field :post,    Types::PostType,    null: true
+    # field :errors,  [String],           null: true
+
+    type Types::PostType
 
     def resolve(id:)
         Post.find(id).destroy
-
     end
 end
