0360050c4b776581508ef5c1de6d4d757f652b83
Mark Sahlgreen <mark@nozzy.org>
2020-04-15 00:52:10 +0200

    organization can be queried from users and users can be queried from organization

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,11 +1,12 @@
 module Types
   class UserType < Types::BaseObject
-    field :id,                ID,                 null: false
-    field :username,          String,             null: true
-    field :organization_id,   Integer,            null: true
-    field :first_name,        String,             null: true
-    field :last_name,         String,             null: true
-    field :email,             String,             null: true
+    field :id,                ID,                         null: false
+    field :username,          String,                     null: true
+    field :organization_id,   Integer,                    null: true
+    field :organization,      Types::OrganizationType,    null: true
+    field :first_name,        String,                     null: true
+    field :last_name,         String,                     null: true
+    field :email,             String,                     null: true
 
   end
 end
