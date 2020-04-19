7bf5d067e480f9b504fb17542f4bb5412e2a9a6c
Mark Sahlgreen <mark@nozzy.org>
2020-04-19 21:18:12 +0200

    removed CreateUser

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,28 +1,26 @@
 module Types
   class MutationType < Types::BaseObject
 
-    # TODO: Create fields for organizations, orders and products 
     # Registering and Signing In
-    field :register_user, mutation: Mutations::RegisterUser
-    field :sign_in, mutation: Mutations::SignIn
+    field :register_user,         mutation: Mutations::RegisterUser
+    field :sign_in,               mutation: Mutations::SignIn
 
     # Creation mutations
-    field :create_user, mutation: Mutations::CreateUser
-    field :create_organization, mutation: Mutations::CreateOrganization
-    field :create_product, mutation: Mutations::CreateProduct
-    field :create_order, mutation: Mutations::CreateOrder
+    field :create_organization,   mutation: Mutations::CreateOrganization
+    field :create_product,        mutation: Mutations::CreateProduct
+    field :create_order,          mutation: Mutations::CreateOrder
 
     # Updating mutations
-    field :update_user, mutation: Mutations::UpdateUser
-    field :update_organization, mutation: Mutations::UpdateOrganization
-    field :update_product, mutation: Mutations::UpdateProduct
-    field :update_order, mutation: Mutations::UpdateOrder
+    field :update_user,           mutation: Mutations::UpdateUser
+    field :update_organization,   mutation: Mutations::UpdateOrganization
+    field :update_product,        mutation: Mutations::UpdateProduct
+    field :update_order,          mutation: Mutations::UpdateOrder
 
 
     # deletion mutations
-    field :destroy_user, mutation: Mutations::DestroyUser
-    field :destroy_organization, mutation: Mutations::DestroyOrganization
-    field :destroy_product, mutation: Mutations::DestroyProduct
-    field :destroy_order, mutation: Mutations::DestroyOrder
+    field :destroy_user,          mutation: Mutations::DestroyUser
+    field :destroy_organization,  mutation: Mutations::DestroyOrganization
+    field :destroy_product,       mutation: Mutations::DestroyProduct
+    field :destroy_order,         mutation: Mutations::DestroyOrder
   end
 end
