c63d1f7fa8d4da2f135bd254db8410eb3d01899c
Mark Sahlgreen <mark@nozzy.org>
2020-04-19 21:18:57 +0200

    fixed an empty line

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,5 @@
 module Types
   class MutationType < Types::BaseObject
-
     # Registering and Signing In
     field :register_user,         mutation: Mutations::RegisterUser
     field :sign_in,               mutation: Mutations::SignIn
