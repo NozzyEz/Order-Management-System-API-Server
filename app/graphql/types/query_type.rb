88d94508886ab3b679b09c267d266fcdb4839e0a
Mark Sahlgreen <mark@nozzy.org>
2020-05-08 12:26:43 +0200

    removed a debugging step from products query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -60,7 +60,6 @@ module Types
 
     def products
       authenticate_user
-      binding.pry
       # Allow admin to see all products, otherwise only show products affiliated with organization
       if current_user.admin?
         Product.all
